667fc9014057a89bc57b61ee32811d6e60720021
Maxim_Groshev <112169730+MaximeGroshev@users.noreply.github.com>
2022-11-17 10:26:40 +0300

    elem by elem comparing

--- main.cpp
+++ main.cpp
@@ -11,54 +11,60 @@ int main()
         //printf("%s\n", command);
         char c = 'o';
         int j = 0;
-        while((c = fgetc(word_com)) != '\n' && c != 32)
+        while(c != '\n')
         {
+            c = fgetc(word_com);
             command [j] = c;
             printf("%c\n", c);
-            if (c == '\n' || c == 32)
-            {
-                printf("us space\n");
-            }
             j++;
 
-        }
-        if (strcmp (command, "push") == 0)
-        {
-            //printf("Hello\n");
-            int value = 0;
-            fscanf(word_com,"%d", &value);
-            fprintf (num_com, "%d %d\n", PUSH, value);
-            printf("%d\n", value);
-            continue;
-        }
+            if (strcmp (command, "push") == 0)
+            {
+                //printf("Hello\n");
+                int value = 0;
+                fscanf(word_com,"%d", &value);
+                fprintf (num_com, "%d %d\n", PUSH, value);
+                printf("%d\n", value);
+                break;
+            }
 
-        command[j - 1] = '\0';
-        if (strcmp (command, "add") == 0)
-        {
-            printf("%s\n", command);
-            fprintf (num_com, "%d\n", ADD);
-            continue;
-        }
+            //command[j - 1] = '\0';
+            if (strcmp (command, "add") == 0)
+            {
+                printf("%s\n", command);
+                fprintf (num_com, "%d\n", ADD);
+                break;
+            }
 
-        else if (strcmp (command, "mul") == 0)
-        {
-            fprintf (num_com, "%d\n", MUL);
-            continue;
-        }
+            else if (strcmp (command, "mul") == 0)
+            {
+                fprintf (num_com, "%d\n", MUL);
+                break;
+            }
 
-        else if (strcmp (command, "out") == 0)
-        {
-            fprintf (num_com, "%d\n", OUT);//pop + print
-            continue;
-        }
+            else if (strcmp (command, "out") == 0)
+            {
+                fprintf (num_com, "%d\n", OUT);//pop + print
+                break;
+            }
 
-        else if (strcmp (command, "hlt") == 0)
+            else if (strcmp (command, "hlt") == 0)
+            {
+                printf("HTL\n");
+                fprintf (num_com, "%d\n", HLT);// exit
+                //free(command);
+                break;
+            }
+        }
+        if (strcmp (command, "hlt") == 0)
         {
-            printf("HTL\n");
-            fprintf (num_com, "%d\n", HLT);// exit
+            //printf("HTL\n");
+            //fprintf (num_com, "%d\n", HLT);// exit
+            //free(command);
+
             free(command);
             break;
-
+        }
         free(command);
     }
     fclose (word_com);
