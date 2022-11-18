acdf6c7c83dd1dfb97942886f834a11da6095f71
Maxim_Groshev <112169730+MaximeGroshev@users.noreply.github.com>
2022-11-18 15:12:32 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,37 +4,36 @@ int main()
 {
     FILE* word_com = fopen ("test.asm", "r");
     FILE* num_com =  fopen ("test.code","a");
-    char* command = (char*) calloc (100, sizeof(char));;
+    char* command = (char*) calloc (100, sizeof (char));
     for (int i = 0;; i++)
     {
-        char c = 'i';
-
-        int j = 0;
-        while(c = fgetc(word_com))
+        printf ("%d\n", i);
+        char c = '\0';
+        int num_of_sym = 0;
+        while (c = fgetc (word_com))
         {
-
             if (c == '\n' || c == 32)
             {
                 break;
             }
-            command [j] = c;
-            printf("%c\n", c);
-            j++;
+            command [num_of_sym] = c;
+            printf ("%c\n", c);
+            num_of_sym++;
         }
 
         if (strcmp (command, "push") == 0)
         {
             int value = 0;
-            fscanf(word_com,"%d", &value);
+            fscanf (word_com,"%d", &value);
             fprintf (num_com, "%d %d\n", PUSH, value);
             printf("%d\n", value);
             continue;
         }
 
-        command[j - 1] = '\0';
+        command [num_of_sym - 1] = '\0';
         if (strcmp (command, "add") == 0)
         {
-            printf("%s\n", command);
+            printf ("%s\n", command);
             fprintf (num_com, "%d\n", ADD);
             continue;
         }
@@ -47,19 +46,19 @@ int main()
 
         else if (strcmp (command, "out") == 0)
         {
-            fprintf (num_com, "%d\n", OUT);//pop + print
+            fprintf (num_com, "%d\n", OUT);
             continue;
         }
 
         else if (strcmp (command, "hlt") == 0)
         {
-            fprintf (num_com, "%d\n", HLT);// exit
-            free(command);
-            fprintf (num_com, "------End of list of commands------\n");
+            fprintf (num_com, "%d\n", HLT);
+            free (command);
             break;
         }
 
     }
+    fprintf (num_com, "------End of list of commands------\n");
     fclose (word_com);
     fclose (num_com);
 }
--- test.asm
+++ test.asm
@@ -1,11 +1,8 @@
-add
 push 10
 push 5
 push 20
 add
-push 12
 mul
 out
-add
 hlt
 
