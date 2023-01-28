3b81651d5ffe9a867b1f6c491965e4a9d7d64f9f
root <root@DESKTOP-KIBV616>
2023-01-28 20:27:11 +0300

    beatiful notification about syntax error

--- assembler/input_err.h
+++ assembler/input_err.h
@@ -1,6 +1,7 @@
-#define INPUT_ERR(token, ...)                                                                                                                \
-        {                                                                                                                               \
-            fprintf (stderr,"Error of input of commands: \033[91m %s  \033[0m\n", token);                                               \
-            fprintf (stderr,"Plese check FILE: %s, number of command %d\n", NAME_OF_PROG, (cur_elem + 1));                              \
-        }                                                                                                                               \
+#define INPUT_ERR(format, ...)                                                                                          \
+        {                                                                                                               \
+            fprintf (stderr,"Error of input of commands: \033[91m ");                                                   \
+            fprintf (stderr, format __VA_OPT__(,) __VA_ARGS__);                                                         \
+            fprintf (stderr,"\033[0m\nPlese check FILE: %s, number of command %d\n", NAME_OF_PROG, (cur_elem + 1));     \
+        }                                                                                                               \
 ;
